565e1ea26c0cb5e2933528333144cc3f57f13c30
milena kurtz <milenatorino@aol.com>
2020-12-02 09:46:27 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// You should NOT change the HTML or CSS in this project (at least until you reach
-// the bonus objectives). Focus on the JavaScript.
+
 
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
@@ -10,7 +9,7 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // this array.
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
@@ -20,19 +19,27 @@ function getCellElements (currentRowElement) {
 }
 
 
-// YOUR CODE GOES HERE
+replaceAllButton.addEventListener("click", function(){
+    let find = findInput.value;
+    let replace = replaceInput.value;
+    //console.log (find, replace)
+    for(let i = 0; i <rowElements.length; i++){
+        //console.log(rowElements[i])
+        let cellElements = getCellElements(rowElements[i]);
+       //console.log(cellElements);
+       for(let j = 0; j<cellElements.length; j++){
+           //console.log (cellElements[j].innerHTML)
+           if(cellElements[j].innerHTML.includes(find)){
+               cellElements[j].innerHTML = cellElements[j].innerHTML.replace(find, replace)
 
 
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
-// You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
+           }
+
+
+       }
+
+    }
+
+})
+
+
